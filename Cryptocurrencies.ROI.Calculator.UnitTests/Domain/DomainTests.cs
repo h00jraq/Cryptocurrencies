66a03cec2645264db3c220001e0fa0b74cae0898
Filip Drzewiecki <fdrzewiecki@objectivity.co.uk>
2021-04-10 19:57:31 +0200

    Add project files.

---
+++ Cryptocurrencies.ROI.Calculator.UnitTests/Domain/DomainTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Cryptocurrencies.ROI.Calculator.UnitTests
+{
+    public class DomainTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
