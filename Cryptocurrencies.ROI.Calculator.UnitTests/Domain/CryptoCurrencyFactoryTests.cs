0b4d57c54074bf4d8757328942c940e09b0dab27
h00jraq <filip.drzewiecki@yahoo.com>
2021-04-25 22:37:39 +0200

    Added one UnitTest for CryptoFactory

--- Cryptocurrencies.ROI.Calculator.UnitTests/Domain/CryptoCurrencyFactoryTests.cs
+++ Cryptocurrencies.ROI.Calculator.UnitTests/Domain/CryptoCurrencyFactoryTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Cryptocurrencies.ROI.Calculator.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,14 +14,20 @@ namespace Cryptocurrencies.ROI.Calculator.UnitTests.Domain
         public void Given_CryptoCurrencyName_When_CreateCryptoCalled_Then_ObjectInstantiated()
         {
             //Arrange
+            var currencyValue = 2100;
+            var blockReward = 4.5m;
+            var networkDifficulty = 45000000;
 
             //Act
-
+            var ethereum = CryptoCurrencyFactory.CreateCrypto(CryptoCurrencyTypes.Ethereum, currencyValue, blockReward, networkDifficulty);
 
             //Assert
-
+            Assert.NotNull(ethereum);
+            Assert.IsType<Ethereum>(ethereum);
 
 
         }
+
+
     }
 }
