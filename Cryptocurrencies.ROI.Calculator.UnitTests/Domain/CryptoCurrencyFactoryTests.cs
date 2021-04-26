ee5dc1e3cd3b474d2f3a815b8c33e26a5023464b
h00jraq <filip.drzewiecki@yahoo.com>
2021-04-26 21:35:07 +0200

    Added more tests for Factory

--- Cryptocurrencies.ROI.Calculator.UnitTests/Domain/CryptoCurrencyFactoryTests.cs
+++ Cryptocurrencies.ROI.Calculator.UnitTests/Domain/CryptoCurrencyFactoryTests.cs
@@ -11,7 +11,7 @@ namespace Cryptocurrencies.ROI.Calculator.UnitTests.Domain
     public class CryptoCurrencyFactoryTests
     {
         [Fact]
-        public void Given_CryptoCurrencyName_When_CreateCryptoCalled_Then_ObjectInstantiated()
+        public void Given_CryptoCurrencyName_When_CreateCryptoCalled_Then_FactoryCreatedEthereum()
         {
             //Arrange
             var currencyValue = 2100;
@@ -25,6 +25,22 @@ namespace Cryptocurrencies.ROI.Calculator.UnitTests.Domain
             Assert.NotNull(ethereum);
             Assert.IsType<Ethereum>(ethereum);
 
+        }
+
+        [Fact]
+        public void Given_CryptoCurrencyName_When_CreateCryptoCalled_Then_FactoryCreatedBitcoin()
+        {
+            //Arrange
+            var currencyValue = 2100;
+            var blockReward = 4.5m;
+            var networkDifficulty = 45000000;
+
+            //Act
+            var bitcoin = CryptoCurrencyFactory.CreateCrypto(CryptoCurrencyTypes.Bitcoin, currencyValue, blockReward, networkDifficulty);
+
+            //Assert
+            Assert.NotNull(bitcoin);
+            Assert.IsType<Bitcoin>(bitcoin);
 
         }
 
