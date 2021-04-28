0890f4a1fb5fefa2ab659ead57f964ab26f91d6a
h00jraq <filip.drzewiecki@yahoo.com>
2021-04-28 23:32:23 +0200

    added new main class for all computing devices, added new class for converting currency, added project for ConsoleClient App

---
+++ ClientApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Cryptocurrencies.ROI.Calculator.Domain;
+
+namespace ClientApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fromCurrency = "PLN";
+            string toCurrency = "USD";
+            string defaultCurrency = "USD";
+            int amount = 5000;
+
+            // Get all available currency tags
+            string[] availableCurrency = CurrencyConverter.GetCurrencyTags();
+            // Print currency tags comma seperated
+            Console.WriteLine("Available Currencies");
+            Console.WriteLine(string.Join(",", availableCurrency));
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Chose your default currency:");
+            defaultCurrency = Console.ReadLine();
+            Console.WriteLine("\n");
+        }
+    }
+}
--- Cryptocurrencies.ROI.Calculator/Domain/ComputingDevice.cs
+++ Cryptocurrencies.ROI.Calculator/Domain/ComputingDevice.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Cryptocurrencies.ROI.Calculator.Domain
 {
-    class ASICcomputingDevice
+    class ComputingDevice
     {
     }
 }
